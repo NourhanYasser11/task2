b519f77b0ed93cc28279cc451a43f93a5331c3d3
NourhanYasser11 <150570952+NourhanYasser11@users.noreply.github.com>
2023-11-12 06:26:02 +0200

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { RatingComponent } from './rating/rating.component';
+import { CoursesComponent } from './courses/courses.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    RatingComponent,
+    CoursesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
